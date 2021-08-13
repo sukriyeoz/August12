fe33aad4f2ce59647c62b4304d3870ea18be1179
sukriyeoz <88262267+sukriyeoz@users.noreply.github.com>
2021-08-12 22:35:45 -0400

    Update Emrah.java

--- src/Emrah.java
+++ src/Emrah.java
@@ -1,6 +1,10 @@
 public class Emrah {
     public static void main(String[] args) {
-        System.out.println("emrah");
+        System.out.println("murat");
+         System.out.println("safiye");
+       System.out.println("surhay");
+         System.out.println("mustafa");    
+        
 
     }
 
